a0ae4759a7b39fd7211ba5b22b36b1d5caa0c96f
Osvaldofrancisco-oc <61027333+Osvaldofrancisco-oc@users.noreply.github.com>
2025-12-19 22:30:27 +0000

    add axios - json-server

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Filter from './components/Filter'; // Assumindo que você criou Filter.jsx
 import PersonForm from './components/PersonForm'; // Assumindo que você criou PersonForm.jsx
 import Persons from './components/Person'; // Assumindo que você criou Persons.jsx
+import axios from 'axios';
+
 
 const App = () => {
+
+  
+
   // --- 1. ESTADO ---
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '123456789', id: '1' },
@@ -11,6 +16,17 @@ const App = () => {
   const [newName, setNewName] = useState('');
   const [newContact, setNewContact] = useState('');
   const [search, setSearch] = useState('');
+
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+        console.log(response.data);
+        console.log('Effect successful');
+      });
+  }, []);
 
   // --- 2. HANDLERS ---
   const handleSearchChange = (e) => {
@@ -49,7 +65,7 @@ const App = () => {
   // Filtra as pessoas com base no input de pesquisa (tanto por nome ou contacto)
   const personsToShow = persons.filter(person => 
     person.number.includes(search) || 
-    person.number.includes(search)
+    person.newName.includes(search)
   );
 
   // --- 4. RENDERIZAÇÃO ---
